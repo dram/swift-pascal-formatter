033a1b6c174694c16d41d5605eb203f9ee16b655
Xin Wang <dram.wang@gmail.com>
2017-05-20 04:42:12 +0000

    Add initial files

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version:4.0
+
+import class PackageDescription.Package
+
+let package = Package(
+    name: "swift-pascal-formatter",
+    targets: [.target(name: "swift-pascal-formatter")]
+)
---
+++ Sources/swift-pascal-formatter/main.swift
@@ -0,0 +1,1 @@
+print("Pascal Formatter in Swift")
